25580fff2cb12f8c70d13ef9157819f90a97de6c
Elliot Sayes <elliot@sayes.cloud>
2024-04-18 13:33:19 +0800

    Setup + load example service worker

---
+++ src/sw.ts
@@ -0,0 +1,29 @@
+/// <reference lib="webworker" />
+
+self.addEventListener(
+  "message",
+  ({ data }) => {
+    console.log("[Service Worker] Message", data);
+    self.postMessage("pong");
+  },
+  false
+);
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install", e);
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] Activate", e);
+});
+
+self.addEventListener("fetch", (e) => {
+  const event = e as FetchEvent;
+  console.log(`[Service Worker] Fetch ${event.request.url}`);
+
+  event.respondWith(
+    (async () => {
+      return fetch(event.request);
+    })()
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: "src",
+      filename: "sw.ts",
+      strategies: "injectManifest",
+      injectRegister: false,
+      manifest: false,
+      injectManifest: {
+        injectionPoint: undefined,
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
 })
