bcd98d172354094cb8ddc4f4d0fcf0a63084b47f
Elliot Sayes <elliot@sayes.cloud>
2024-04-19 21:50:57 +0800

    path resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from "vite-plugin-pwa"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,4 +22,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
