ee12ef2c9cbcfb5470d71ad658e22c89ca36bfd1
Elliot Sayes <elliot@sayes.cloud>
2024-04-19 17:35:17 +0800

    Fix positioning + interactability

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,4 @@
+import { AlphaFilter } from "pixi.js";
 import { Stage } from "@pixi/react";
 import { Container } from "@pixi/react-animated";
 import { Spring } from "@react-spring/web";
@@ -7,6 +8,7 @@ import { gameMachine } from "../lib/machines/game";
 import { useMachine } from "@xstate/react";
 import InteractableToon from "./InteractableToon";
 import NamedAvatar from "./NamedAvatar";
+import { useMemo } from "react";
 
 const tileSizeX = 64;
 const tileSizeY = 64;
@@ -24,6 +26,9 @@ export const Game = ({ aoStateProp: aoState }: Props) => {
 	const [current, send] = useMachine(gameMachine, {
 		id: "game",
 	});
+
+  const veryTransparent = useMemo(() => new AlphaFilter(0.3), []);
+  const slightlyTransparent = useMemo(() => new AlphaFilter(0.6), []);
 
 	return (
 		<>
@@ -45,32 +50,65 @@ export const Game = ({ aoStateProp: aoState }: Props) => {
 				{current.hasTag("SHOW_WORLD") && (
 					<Spring
 						to={{
-							x: -current.context.currentPosition.x * tileSizeX + tileSizeX / 2,
-							y: -current.context.currentPosition.y * tileSizeY + tileSizeY / 2,
+							x: -((current.context.currentPosition.x + 1) * tileSizeX - stageWidth / 2) + tileSizeX / 2,
+							y: -((current.context.currentPosition.y + 1) * tileSizeY - stageHeight / 2) + tileSizeY / 2,
 						}}
 					>
 						{(props) => (
 							<Container anchor={{ x: 0.5, y: 0.5 }} {...props}>
 								<Tilemap3 />
 								{current.hasTag("SHOW_OTHER_TOONS") &&
-									aoState.otherToons.map((toon) => (
-										<InteractableToon
-											key={toon.id}
-											name={toon.displayName}
-											seed={toon.avatarSeed}
-											scale={3}
-											x={toon.savedPosition.x * tileSizeX + tileSizeX / 2}
-											y={toon.savedPosition.y * tileSizeY + tileSizeY / 2}
-											isPlaying={true}
-											animationName={"idle"}
-											animationSpeed={
-												current.context.selectedToonId === toon.id ? 0.2 : 0.05
-											}
-											onclick={() =>
-												send({ type: "TOON_SELECTED", toonId: toon.id })
-											}
-										/>
-									))}
+									aoState.otherToons.map((toon) => {
+										// check if within two tiles
+										const { x: selfX, y: selfY } =
+											current.context.currentPosition;
+										const { x: otherX, y: otherY } = toon.savedPosition;
+
+										const withinThreeTiles =
+											Math.abs(selfX - otherX) <= 3 &&
+											Math.abs(selfY - otherY) <= 3;
+
+										if (withinThreeTiles) {
+											return (
+												<InteractableToon
+													key={toon.id}
+													name={toon.displayName}
+													seed={toon.avatarSeed}
+													scale={3}
+													x={toon.savedPosition.x * tileSizeX + tileSizeX / 2}
+													y={toon.savedPosition.y * tileSizeY + tileSizeY / 2}
+													isPlaying={true}
+													animationName={"idle"}
+													animationSpeed={
+														current.context.selectedToonId === toon.id
+															? 0.3
+															: 0.1
+													}
+													onclick={() =>
+														send({ type: "TOON_SELECTED", toonId: toon.id })
+													}
+												/>
+											);
+										} else {
+                      const withinFiveTiles =
+                        Math.abs(selfX - otherX) <= 5 &&
+                        Math.abs(selfY - otherY) <= 5;
+                      return (
+												<NamedAvatar
+													key={toon.id}
+													name={toon.displayName}
+													seed={toon.avatarSeed}
+													scale={3}
+													x={toon.savedPosition.x * tileSizeX + tileSizeX / 2}
+													y={toon.savedPosition.y * tileSizeY + tileSizeY / 2}
+													isPlaying={true}
+													animationName={"idle"}
+													animationSpeed={0.05}
+                          filters={withinFiveTiles ? [slightlyTransparent] : [veryTransparent]}
+												/>
+											);
+										}
+									})}
 							</Container>
 						)}
 					</Spring>
@@ -79,7 +117,7 @@ export const Game = ({ aoStateProp: aoState }: Props) => {
 				{current.hasTag("SHOW_TOON") && (
 					<Container x={stageWidth / 2} y={stageHeight / 2}>
 						<NamedAvatar
-              name={aoState.user.displayName}
+							name={aoState.user.displayName}
 							seed={aoState.user.avatarSeed}
 							animationName={
 								current.matches({
