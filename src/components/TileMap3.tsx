3782b4d08f8c197b9a7890d13e33e6dd5a305d97
Elliot Sayes <elliot@sayes.cloud>
2024-04-21 22:23:58 +0800

    Fix room generation

--- src/components/TileMap3.tsx
+++ src/components/TileMap3.tsx
@@ -15,6 +15,8 @@ export const roomTilesY = 13;
 export const blockSpacingX = 2;
 export const blockSpacingY = 4;
 
+export const windowSpacing = 4;
+
 export const Tilemap3 = PixiComponent("TileMap3", {
 	create: () => {
 		return new CompositeTilemap();
@@ -29,18 +31,63 @@ export const Tilemap3 = PixiComponent("TileMap3", {
 		// if you are too lazy, just specify filename and pixi will find it in cache
 		for (let x = 0; x < roomTilesX; x++) {
 			for (let y = 0; y < roomTilesY; y++) {
-				instance.tile("room_3_2", x * tileSize, y * tileSize);
-
-				if (x % blockSpacingX === 0 && y % blockSpacingY === 0) {
-					instance.tile("room_2_2", x * tileSize, y * tileSize);
+				if (x >= 1 && x < roomTilesX - 1 && y >= 2 && y < roomTilesY - 1) {
+					instance.tile("room_3_2", x * tileSize, y * tileSize);
 				}
 
-				if (y === roomTilesY - 1) {
-					instance.tile("room_3_0", x * tileSize, y * tileSize);
-				} else if (x === 0 || x === roomTilesX - 1) {
-					instance.tile("room_3_0", x * tileSize, y * tileSize);
-				} else if (y === 0) {
-					instance.tile("room_3_0", x * tileSize, y * tileSize);
+				// Replace trees with sprite
+				// if (x % blockSpacingX === 0) {
+				// 	instance.tile("room_2_2", x * tileSize, y * tileSize);
+				// }
+
+				if (x === 0) {
+					if (y === 0) {
+						instance.tile("room_0_0", x * tileSize, y * tileSize);
+					} else if (y === roomTilesY - 1) {
+						instance.tile("room_0_3", x * tileSize, y * tileSize);
+					} else {
+						instance.tile("room_0_2", x * tileSize, y * tileSize);
+					}
+				} else if (x === 1) {
+					if (y === 0) {
+						instance.tile("room_1_0", x * tileSize, y * tileSize);
+					} else if (y === 1) {
+						instance.tile("room_1_1", x * tileSize, y * tileSize);
+					} else if (y === roomTilesY - 1) {
+						instance.tile("room_1_3", x * tileSize, y * tileSize);
+					} else {
+						instance.tile("room_1_2", x * tileSize, y * tileSize);
+					}
+				} else if (x === roomTilesX - 1) {
+					if (y === 0) {
+						instance.tile("room_4_0", x * tileSize, y * tileSize);
+					} else if (y === roomTilesY - 1) {
+						instance.tile("room_4_3", x * tileSize, y * tileSize);
+					} else {
+						instance.tile("room_4_2", x * tileSize, y * tileSize);
+					}
+				} else if (x % windowSpacing === 0) {
+					if (y === 0) {
+						instance.tile("room_2_0", x * tileSize, y * tileSize);
+					} else if (y === 1) {
+						instance.tile("room_2_1", x * tileSize, y * tileSize);
+					}  else if (y === 2) {
+						instance.tile("room_2_2", x * tileSize, y * tileSize);
+					} else if (y === roomTilesY - 1) {
+						instance.tile("room_2_3", x * tileSize, y * tileSize);
+					} else {
+						instance.tile("room_3_2", x * tileSize, y * tileSize);
+					}
+				} else {
+					if (y === 0) {
+						instance.tile("room_3_0", x * tileSize, y * tileSize);
+					} else if (y === 1) {
+						instance.tile("room_3_1", x * tileSize, y * tileSize);
+					} else if (y === roomTilesY - 1) {
+						instance.tile("room_3_3", x * tileSize, y * tileSize);
+					} else {
+						instance.tile("room_3_2", x * tileSize, y * tileSize);
+					}
 				}
 			}
 		}
