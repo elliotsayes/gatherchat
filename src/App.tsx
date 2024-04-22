30652d404bdf533d21b6b1f078da99badc7e52fc
Elliot Sayes <elliot@sayes.cloud>
2024-04-22 21:57:36 +0800

    Ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { GameDemo } from "./components/game/GameDemo";
+import { GameDemo } from "./components/GameDemo";
 import { queryClient } from "./lib/query";
 
 function App() {
