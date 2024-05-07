e48df9198c2bb19b3b490a33359b7d1c638bcb39
Elliot Sayes <elliot@sayes.cloud>
2024-05-07 19:15:28 +0900

    Detech unusud

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from "vite-plugin-pwa"
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import deadFile from 'vite-plugin-deadfile';
 import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    deadFile({
+      root: 'src',
+    }),
     react(),
     VitePWA({
       srcDir: "src",
