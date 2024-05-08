801ce810722160f80298a0b08ec429ccd1c49a51
Elliot Sayes <elliot@sayes.cloud>
2024-05-08 19:12:55 +0900

    Clean

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,12 +9,22 @@ export const Route = createRootRoute({
 				<div>
 					<div className="fixed bottom-0 left-0 p-2 bg-black text-white z-50">
 						<p className="font-bold text-xl">Running in Development Mode</p>
-						<p>See routes @ <pre>src/routes/*</pre></p>
-						<p>Edit this file at <pre>src/routes/__root.tsx</pre></p>
+						<p>
+							See routes @ <pre>src/routes/*</pre>
+						</p>
+						<p>
+							Edit this file at <pre>src/routes/__root.tsx</pre>
+						</p>
 						<div className="flex flex-row gap-4">
-							<Link className="bg-gray-500" to="/">Home</Link>
-							<Link className="bg-gray-500" to="/game">Game</Link>
-							<Link className="bg-gray-500" to="/render">Render Demo</Link>
+							<Link className="bg-gray-500" to="/">
+								Home
+							</Link>
+							<Link className="bg-gray-500" to="/game">
+								Game
+							</Link>
+							<Link className="bg-gray-500" to="/render">
+								Render Demo
+							</Link>
 						</div>
 					</div>
 				</div>
